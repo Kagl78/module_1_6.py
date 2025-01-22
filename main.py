033085a6feaf8802bbdf6ca5327ac1edcd109431
KarimAgliullin <agliullinkarim1@gmail.com>
2025-01-22 20:05:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 'go', 'lvt')
+print("Immutable tuple:", immutable_var)
+immutable_var[0] = 8
+mutable_list = (1, 4, 7, 8)
+print(mutable_list)
+mutable_list[2] = 7
+print(mutable_list)
